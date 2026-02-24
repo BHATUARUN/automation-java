f4746aa1b296f71de3db25af910359479e06abb6
Bhatu Arun <ahirarun845@gmail.com>
2026-02-24 17:33:50 +0530

    Add files via upload

---
+++ assignment/assignq16.java
@@ -0,0 +1,15 @@
+package test;
+
+public class assignq16 {
+    public static void main(String[] args) {
+
+        String s1 = "Hello";
+        String s2 = "Hello";
+
+        if(s1.equals(s2)) {
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+    }
+}
