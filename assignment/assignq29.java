f69e68a6f6224c172ef5ef6c3eeccbb654a9490d
Bhatu Arun <ahirarun845@gmail.com>
2026-02-24 18:33:37 +0530

    Add files via upload

--- assignment/assignq29.java
+++ assignment/assignq29.java
@@ -2,16 +2,14 @@ package test;
 abstract class Parent {
     abstract void message();
 }
-
 class Child1 extends Parent {
     void message() {
-        System.out.println("Hello from Child1");
+        System.out.println("This is first subclass");
     }
 }
-
 class Child2 extends Parent {
     void message() {
-        System.out.println("Hello from Child2");
+        System.out.println("This is second subclass");
     }
 }
 
@@ -19,7 +17,6 @@ public class assignq29 {
     public static void main(String[] args) {
         Parent obj1 = new Child1();
         Parent obj2 = new Child2();
-
         obj1.message();
         obj2.message();
     }
